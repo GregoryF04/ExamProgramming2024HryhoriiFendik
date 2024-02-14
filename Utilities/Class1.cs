d8b31b86abbc8a5a3698b97b1e05ad048c9e2072
Hryhorii Fendik <w68920@student.wsiz.edu.pl>
2024-02-14 21:45:27 +0100

    Task 3

---
+++ Utilities/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace Utilities
+{
+    public static class StringUtilities
+    {
+        public static int LongWordsCount(string input)
+        {
+            string[] words = input.Split(' ');
+            return words.Count(word => word.Length > 2);
+        }
+
+        public static string ToSpinalCase(string input)
+        {
+            string[] words = input.Split(' ');
+            return string.Join("-", words.Select(word => word.ToLower()));
+        }
+
+        public static string ToPascalCase(string input)
+        {
+            string[] words = input.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = char.ToUpper(words[i][0]) + words[i][1..].ToLower();
+            }
+            return string.Join("", words);
+        }
+    }
+}
