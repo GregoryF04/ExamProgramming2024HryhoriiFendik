94b4f00caf8201d60b22a262a8819551f1a39d2f
Hryhorii Fendik <w68920@student.wsiz.edu.pl>
2024-02-14 21:08:19 +0100

    Task 2

---
+++ Geometry/Class1.cs
@@ -0,0 +1,37 @@
+﻿namespace Geometry
+{
+    public interface IReflectable
+    {
+        void ReflectX();
+        void ReflectY();
+    }
+    public class Point : IReflectable
+    {
+        private decimal _x;
+        private decimal _y;
+
+        public enum PointColour
+        {
+            Red,
+            Green,
+            Blue
+        }
+
+        public decimal X => _x;
+        public decimal Y => _y;
+        public Point(decimal x)
+        {
+            _x = x;
+            _y = 0;
+        }
+
+        public Point(decimal x, decimal y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void ReflectX() => _x = -_x;
+        public void ReflectY() => _y = -_y;
+    }
+}
---
+++ Geometry/ColourfulPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+    public class ColourfulPoint : Point, IReflectable
+    {
+        public PointColour Colour { get; set; }
+
+        public ColourfulPoint(decimal x, decimal y, PointColour colour) : base(x, y)
+        {
+            Colour = colour;
+        }
+        public void ChangeColour(PointColour newColour)
+        {
+            Colour = newColour;
+        }
+        public void NextColour()
+        {
+            Colour = (PointColour)(((int)Colour + 1) % 3);
+        }
+    }
+}
